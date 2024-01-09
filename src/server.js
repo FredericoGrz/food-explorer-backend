4d3c38751b46f8704fbeeca380e54c649751b4bf
FredericoGrz <frederico.graz@gmail.com>
2024-01-09 12:30:35 -0300

    Adicionado todas as rotas na api e middleware para controle de erros

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 require("express-async-errors");
 require("dotenv/config");
 const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
 const PORT = process.env.SERVER_PORT;
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(routes);
+
+// Middleware para controle de erros na aplicação
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log("Running server on port", PORT));
