ba9ecd96a6f3ac00ace952b60e88cd732f172f69
FredericoGrz <frederico.graz@gmail.com>
2024-01-15 14:40:01 -0300

    Adicionado a restricao de autenticidade e de admin para o upload de imagens

--- src/routes/imageUpload.router.js
+++ src/routes/imageUpload.router.js
@@ -1,12 +1,18 @@
 const { Router } = require("express");
 const multer = require("multer");
 const uploadConfig = require("../configs/upload");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const ensureIsAdmin = require("../middlewares/ensureIsAdmin");
 
 const imageUploadRoute = Router();
 const imgUpload = multer(uploadConfig.MULTER);
 
-imageUploadRoute.post("/", imgUpload.single("image"), (req, res) =>
-  res.status(200).json({ file: req.file.filename })
+imageUploadRoute.post(
+  "/",
+  ensureAuthenticated,
+  ensureIsAdmin,
+  imgUpload.single("image"),
+  (req, res) => res.status(200).json({ file: req.file.filename })
 );
 
 module.exports = imageUploadRoute;
